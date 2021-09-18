2a06cdcc5cbaab79731c905b281145e1f43f19f2
jcorozco81 <jcorozco@gmail.com>
2021-09-18 01:46:24 -0400

    Removed adding an ID for new exercises array elements

--- models/WorkoutSchema.js
+++ models/WorkoutSchema.js
@@ -12,18 +12,60 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    day: { type: Date, default: Date.now },
-    exercises:[{ 
-        type: String, 
-        name: String, 
-        duration: Number,
-        weight: Number,
-        reps: Number,
-        sets: Number,
-    }]
+  day: {
+    type: Date,
+    unique: true,
+    default: Date.now,
+  },
+  exercises: [
+    {      
+        _id: false,
+
+      type: {
+        type: String,
+        required: "Workout type is required"
+    },
+      name: {
+        type: String,
+        trim: true,
+        required: "Workout name is required"
+
+      },
+      duration: {
+        type: Number,
+        trim: true,
+        required: "Duration is required"
+      },
+
+      distance:{
+        type: Number,
+        trim: true,
+        required: "Distance is required" 
+      },
+
+
+      weight: {
+        type: Number,
+        trim: true,
+        required: "A number for weight is required"
+
+      },
+      reps: {
+        type: Number,
+        trim: true,
+        required: "A number of reps is required"
+
+      },
+
+      sets: {
+        type: Number,
+        trim: true,
+        required: "Number of sets is required",
+      },
+    },
+  ],
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
-
