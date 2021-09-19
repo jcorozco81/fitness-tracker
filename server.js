8151e0cdffa7695f285f34bf436a186c8c5b4e90
jcorozco81 <jcorozco@gmail.com>
2021-09-18 23:51:30 -0400

    Added additional MongoDB configuration parameters

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const path = require('path');
 
 
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3000;
 const routes = require('./routes');
 const app = express();
 
@@ -16,7 +16,13 @@ app.use(express.static("public"));
 
 
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
+{
+useNewUrlParser: true,
+useUnifiedTopology: true,
+useCreateIndex: true,
+useFindAndModify: false
+});
 
 app.use(routes);
 
